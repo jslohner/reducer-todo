dd9efee3890d4e8e8b35fb501e91daeb69534a79
Justin Lohner <lohner4@gmail.com>
2020-05-11 23:59:25 -0600

    Added some styling to the website

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useReducer } from 'react';
 
+import { initialState, reducer } from './reducers/reducer.js';
+
+import './App.css';
 import TodoList from './components/TodoList.js';
+import TodoForm from './components/TodoForm.js';
 
 function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
 	return (
 		<div className='App'>
-			<TodoList />
+			<TodoList state={state} dispatch={dispatch}/>
+			<TodoForm state={state} dispatch={dispatch}/>
 		</div>
 	);
 }
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,26 +1,20 @@
-import React, { useReducer } from 'react';
-
-import { initialState, reducer } from '../reducers/reducer.js';
+import React from 'react';
 
 import Todo from './Todo.js';
-import TodoForm from './TodoForm.js';
 
-function TodoList() {
-	const [state, dispatch] = useReducer(reducer, initialState);
-
+function TodoList({ state, dispatch }) {
 	const toggleComplete = task => {
 		dispatch({ type: 'TOGGLE_COMPLETE', payload: task });
 	}
 
 	return (
-		<div>
-			<h2>TodoList</h2>
+		<div className='todo-list'>
+			<h2>Todo List</h2>
 			{
 				state.taskList.map(task => {
 					return <Todo key={task.id} toggleComplete={toggleComplete} task={task}/>
 				})
 			}
-			<TodoForm state={state} dispatch={dispatch}/>
 		</div>
 	);
 }
